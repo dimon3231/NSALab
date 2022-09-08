0122727db8641a2203b4304c7722982488199b39
Sokolov Georgii <npl1u2pc@mail.ru>
2022-09-08 21:33:38 +0700

    new file:   createvm.tf 	modified:   uploadvdmk.tf

--- uploadvdmk.tf
+++ uploadvdmk.tf
@@ -5,3 +5,10 @@ resource "yandex_storage_object" "test-object" {
   key        = "cml2.vmdk"
   source     = "D:/VM/CML2/cml240_rev2-disk1.vmdk"
 }
+
+resource "yandex_compute_image" "image-1" {
+  name       = "CML2"
+  os_type    = "LINUX"
+  source_url = "https://storage.yandexcloud.net/bucket1/cml2.vmdk"
+  pooled     = "false"
+}
