55e951d33c6fa2d463680370740f107abeaf0920
Sokolov Georgii <npl1u2pc@mail.ru>
2022-09-08 21:14:11 +0700

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -21,27 +21,3 @@ resource "yandex_resourcemanager_cloud_iam_binding" "admin" {
     "serviceAccount:ajelguvuftg771n1hih6",
   ]
 }
-
-resource "yandex_compute_instance" "vm-1" {
-  name = "CML2"
-
-  resources {
-    cores  = 4
-    memory = 8
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd87va5cc00gaq2f5qfb"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
