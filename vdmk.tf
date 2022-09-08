519fe6a36c7029a469e2a6286dccbc3e4f7ad09a
Sokolov Georgii <npl1u2pc@mail.ru>
2022-09-08 22:35:42 +0700

    modified:   vdmk.tf

--- vdmk.tf
+++ vdmk.tf
@@ -20,4 +20,5 @@ resource "yandex_storage_bucket" "test" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   bucket     = "bucket1"
+  max_size   = 0
 }
